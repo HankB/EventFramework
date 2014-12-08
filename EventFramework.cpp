0fe6b5f56af0ee7ee9ca8befec36d0dbfe8db2bf
Hank Barta <hbarta@gmail.com>
2014-12-07 22:48:28 -0600

    Testing as a library

--- EventFramework.cpp
+++ EventFramework.cpp
@@ -1,4 +1,5 @@
 #include "EventFramework.h"
+namespace efl {
 
 template<>
 void LL<Event>::doItems()
@@ -8,3 +9,103 @@ void LL<Event>::doItems()
       pLL = pLL->erase();         // remove from list
 }
 
+template<>
+void LL<Timer>::doItems()
+{
+  static ulong prevMillis = 0;    // start when execution starts
+  ulong   nowMillis = millis();   // time now
+  ulong   deltaMillis = nowMillis - prevMillis;
+
+  if(!deltaMillis)
+    return;
+
+  // iterate through timers to see which ones have down counted to or beyond zero
+  for(LL<Timer>* pLL = begin(); pLL != end(); )
+  {
+    ulong late=deltaMillis - pLL->pItem->getCounter();
+    if( pLL->pItem->getCounter() <= deltaMillis )
+    {
+      if( pLL->pItem->callback(late) && pLL->pItem->getPeriod() > 0 ) // need both period and 'true' response to keep active
+      {
+        // policy decision here. Do we set the counter to 0 or less if
+        // we're late by the period or more? No, I guess...
+        if( late >= pLL->pItem->getPeriod())
+          pLL->pItem->setCounter(1);
+        else
+          pLL->pItem->setCounter(pLL->pItem->getPeriod()-late);
+        pLL = pLL->next();
+      }
+      else
+      {
+        pLL = pLL->erase();
+      }
+    }
+    else
+    {
+      pLL->pItem->setCounter(pLL->pItem->getCounter()-deltaMillis);
+      pLL = pLL->next();
+    }
+  }
+  prevMillis = nowMillis;
+}
+
+template<>
+void LL<Digital>::doItems() {
+     static ulong prevMillis = 0;    // start when execution starts
+     ulong   nowMillis = millis();   // time now
+     uint   deltaMillis = nowMillis - prevMillis;
+ 
+     if(!deltaMillis)
+       return;
+ 
+ // iterate through timers to see which ones have downcounted to or beyond zero
+     for(LL<Digital>* pLL = begin(); pLL != end(); pLL = pLL->next()) {
+       // scan all digital inputs
+         switch(pLL->pItem->getState()) {
+             case Digital::INACTIVE:
+                if( pLL->pItem->getSense()) {
+                    //Printf( "pItem->id(%d) going active.\n", pLL->pItem->getID());
+                    if( pLL->pItem->getDebounce() > 0 ) {
+                        pLL->pItem->setState(Digital::GOING_ACTIVE);
+                        pLL->pItem->setDebounceCounter();
+                    }
+                    else {
+                        pLL->pItem->setState(Digital::ACTIVE);
+                    }
+                }
+                break;
+
+             case Digital::GOING_INACTIVE:
+             case Digital::GOING_ACTIVE:
+                if( pLL->pItem->decrementDebounce(deltaMillis) <= 0) {
+                    //Printf("Debounce complete id(%d)\n", pLL->pItem->getID());
+                    if( pLL->pItem->getSense() )
+                        pLL->pItem->setState(Digital::ACTIVE);
+                    else
+                        pLL->pItem->setState(Digital::INACTIVE);
+                 }
+                 break;
+
+             case Digital::ACTIVE:
+                if( !pLL->pItem->getSense()) {
+                    //Printf( "pItem->id(%d) going inactive.\n", pLL->pItem->getID());
+                    if( pLL->pItem->getDebounce() > 0 ) {
+                        pLL->pItem->setState(Digital::GOING_INACTIVE);
+                        pLL->pItem->setDebounceCounter();
+                    }
+                    else {
+                        pLL->pItem->setState(Digital::INACTIVE);
+                    }
+                }
+                 break;
+
+             default:
+                 pLL->pItem->setState(Digital::INACTIVE); // what else to do here?
+                 break;
+          }
+      }
+    prevMillis = nowMillis;
+}
+
+
+} // namespace efl
--- EventFramework.h
+++ EventFramework.h
@@ -1,3 +1,9 @@
+#if !defined EVENTFRAMEWORK_H
+#define EVENTFRAMEWORK_H
+#include <Arduino.h>
+
+// debugging 
+
 namespace efl { // event framework library
 
 /**
@@ -144,7 +150,6 @@ LL<Item>* LL<Item>::erase()
     }
     pLL = pLL->pNext;
   }
-  assert(0);
   return pLL->pNext;      // should never get here
 }
 
@@ -158,7 +163,6 @@ public:
   Event() {
   };
   virtual bool callback() {
-    if (verbose) coln( "Event:");
     return false;
   };
 };
@@ -178,9 +182,6 @@ public:
   counter(c),period(p) {
   }; // default to fire once after 1 ms
   virtual bool callback(ulong late) {
-    if (verbose) {
-      coln( "Timer:");
-    }
     return false;
   };
   ulong getCounter() {
@@ -197,48 +198,6 @@ public:
   }
 
 };
-
-template<>
-void LL<Timer>::doItems()
-{
-  static ulong prevMillis = 0;    // start when execution starts
-  ulong   nowMillis = millis();   // time now
-  ulong   deltaMillis = nowMillis - prevMillis;
-
-  if(!deltaMillis)
-    return;
-
-  // iterate through timers to see which ones have down counted to or beyond zero
-  for(LL<Timer>* pLL = begin(); pLL != end(); )
-  {
-    ulong late=deltaMillis - pLL->pItem->getCounter();
-    if( pLL->pItem->getCounter() <= deltaMillis )
-    {
-      if( pLL->pItem->callback(late) && pLL->pItem->getPeriod() > 0 ) // need both period and 'true' response to keep active
-      {
-        // policy decision here. Do we set the counter to 0 or less if
-        // we're late by the period or more? No, I guess...
-        if( late >= pLL->pItem->getPeriod())
-          pLL->pItem->setCounter(1);
-        else
-          pLL->pItem->setCounter(pLL->pItem->getPeriod()-late);
-        pLL = pLL->next();
-      }
-      else
-      {
-        pLL = pLL->erase();
-      }
-    }
-    else
-    {
-      pLL->pItem->setCounter(pLL->pItem->getCounter()-deltaMillis);
-      pLL = pLL->next();
-    }
-  }
-  prevMillis = nowMillis;
-}
-#define DIGITAL
-#if defined DIGITAL
 
 /**
  * Provide a framework to respond to changes in state of digital inputs including 
@@ -287,7 +246,6 @@ public:
  
 
 private:
-  int           id;
   uint          debounce;
   int           debounceCounter;
   States        state;
@@ -298,8 +256,8 @@ private:
   
 
 public:
-  Digital( int id, DigitalBit b,int d = 1, Polarity p = ACT_HI, uchar interest = (INACTIVE|ACTIVE)):
-  id(id), debounce(d), state(INACTIVE), polarity(p), pin(b), interestMask(interest)
+  Digital( const DigitalBit b,int d = 1, Polarity p = ACT_HI, uchar interest = (INACTIVE|ACTIVE)):
+  debounce(d), state(INACTIVE), polarity(p), pin(b), interestMask(interest)
   {
      pinMode(pin, INPUT);      // should this be done in setup?
   };				// defaults: 1 ms debounce and active high polarity
@@ -318,80 +276,16 @@ public:
           callback(0, state, oldState);
   };
   bool getSense() { return (polarity==ACT_HI)?digitalRead(pin):!digitalRead(pin); };
-  int getID() { return id; };
+  int getID() { return pin; };
   uint setDebounceCounter() { return debounceCounter = debounce; };
   uint getDebounce() { return debounce; };
   int decrementDebounce(uint delta) { return debounceCounter -= delta; };
   virtual bool callback (ulong late, States newState, States oldState) {   /// callback on state changes
-
-    if (verbose)
-	  coln (X("Digital:"));
     return false;
   };
 };
 
-template<>
-void LL<Digital>::doItems() {
-     static ulong prevMillis = 0;    // start when execution starts
-     ulong   nowMillis = millis();   // time now
-     uint   deltaMillis = nowMillis - prevMillis;
- 
-     if(!deltaMillis)
-       return;
- 
-//     if(verbose) walk();
-     
- // iterate through timers to see which ones have downcounted to or beyond zero
-     for(LL<Digital>* pLL = begin(); pLL != end(); pLL = pLL->next()) {
-       // scan all digital inputs
-         switch(pLL->pItem->getState()) {
-             case Digital::INACTIVE:
-                if( pLL->pItem->getSense()) {
-                    Printf( "pItem->id(%d) going active.\n", pLL->pItem->getID());
-                    if( pLL->pItem->getDebounce() > 0 ) {
-                        pLL->pItem->setState(Digital::GOING_ACTIVE);
-                        pLL->pItem->setDebounceCounter();
-                    }
-                    else {
-                        pLL->pItem->setState(Digital::ACTIVE);
-                    }
-                }
-                break;
-
-             case Digital::GOING_INACTIVE:
-             case Digital::GOING_ACTIVE:
-                if( pLL->pItem->decrementDebounce(deltaMillis) <= 0) {
-                    Printf("Debounce complete id(%d)\n", pLL->pItem->getID());
-                    if( pLL->pItem->getSense() )
-                        pLL->pItem->setState(Digital::ACTIVE);
-                    else
-                        pLL->pItem->setState(Digital::INACTIVE);
-                 }
-                 break;
-
-             case Digital::ACTIVE:
-                if( !pLL->pItem->getSense()) {
-                    Printf( "pItem->id(%d) going inactive.\n", pLL->pItem->getID());
-                    if( pLL->pItem->getDebounce() > 0 ) {
-                        pLL->pItem->setState(Digital::GOING_INACTIVE);
-                        pLL->pItem->setDebounceCounter();
-                    }
-                    else {
-                        pLL->pItem->setState(Digital::INACTIVE);
-                    }
-                }
-                 break;
-
-             default:
-                 pLL->pItem->setState(Digital::INACTIVE); // what else to do here?
-                 break;
-          }
-      }
-    prevMillis = nowMillis;
-}
-
-#endif //defined DIGITAL
-
 } // namespace efl
 
+#endif // !defined EVENTFRAMEWORK_H
 
